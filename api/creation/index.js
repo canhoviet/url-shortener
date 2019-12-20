1732f1f09ac83e63769e734dc703d0ac61a73b33
Can Ho <vietcanho@gmail.com>
2019-12-20 22:52:58 +0700

    update config name

--- api/creation/index.js
+++ api/creation/index.js
@@ -2,13 +2,13 @@ const Boom = require('@hapi/boom');
 const { shorten } = require('./creation.svc');
 const logger = require('../../utils/logger');
 
-const { SERVER_HOST, SERVER_PORT } = process.env;
+const { HOST, PORT, PROTO } = process.env;
 exports.create = async function(request, h) {
     const longURL = request.query.url;
     try {
         const shortURLId = await shorten(longURL);
         return {
-            url: `${SERVER_HOST}:${SERVER_PORT}/${shortURLId}`
+            url: `${PROTO}://${HOST}:${PORT}/${shortURLId}`
         };
     } catch (e) {
         logger.error(e);
--- config/manifest.js
+++ config/manifest.js
@@ -1,29 +1,8 @@
-const envKey = key => {
-    const env = process.env.NODE_ENV || 'development';
-
-    const configuration = {
-        development: {
-            host: 'localhost',
-            port: 3000
-        },
-        test: {
-            host: 'localhost',
-            port: 8010
-        },
-        // These should match environment variables on hosted server
-        production: {
-            host: process.env.HOST,
-            port: process.env.PORT
-        }
-    };
-
-    return configuration[env][key];
-};
-
+const { HOST, PORT } = process.env;
 const manifest = {
     server: {
-        host: envKey('host'),
-        port: envKey('port'),
+        host: HOST,
+        port: PORT,
         routes: {
             cors: true
         },
